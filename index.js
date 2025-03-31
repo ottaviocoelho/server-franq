f12125c15fdf4416ed8202342ceb39b31918a8b7
ottaviocoelho <ottaviocoelho@gmail.com>
2025-03-31 00:46:49 -0300

    Cors

--- index.js
+++ index.js
@@ -2,26 +2,39 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const NodeCache = require('node-cache');
+const cors = required('cors')
 
 const app = express();
-const cache = new NodeCache({ stdTTL: 60 });
+const cache = new NodeCache({ stdTTL: 9999999 });
 const PORT = process.env.PORT || 3000;
 const API_URL = 'https://api.hgbrasil.com/finance';
 const API_KEY = process.env.API_KEY;
+
+const updateCacheWithLogging = (key, newValue) => {
+  const currentValue = cache.get(key);
+
+  if (JSON.stringify(currentValue) !== JSON.stringify(newValue)) {
+    cache.set(key, newValue);
+
+    console.log(`[${new Date().toISOString()}] Cache atualizado para a chave "${key}". Novo valor:`, newValue);
+  } else {
+    console.log(`[${new Date().toISOString()}] Valor para a chave "${key}" não mudou. Nenhuma atualização no cache.`);
+  }
+};
 
 // Cache
 async function fetchFinanceData() {
     try {
         const response = await axios.get(API_URL, { params: { key: API_KEY } });
         const data = response.data.results;
-        cache.set('finance', data);
+        updateCacheWithLogging('finance', data)
         console.log('Updated cache: ', new Date().toISOString());
     } catch (error) {
         console.error('Error on retrieving data: ', error.message);
     }
 }
 
-setInterval(fetchFinanceData, 30000);
+//setInterval(fetchFinanceData, 60000);
 fetchFinanceData();
 
 function sendCachedData(req, res, key) {
@@ -32,6 +45,17 @@ function sendCachedData(req, res, key) {
       res.status(204).send();
   }
 }
+
+const allowedOrigins = [
+  "http://localhost:3000", // Localhost para desenvolvimento
+  process.env.HEROKU_FRONTEND_URL // URL do frontend no Heroku (definida no Heroku)
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 // Endpoints for client
 app.get('/currencies', (req, res) => sendCachedData(req, res, 'currencies'));
